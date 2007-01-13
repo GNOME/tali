dec45f655de39aab32b9b30c95f074eb19ac039d
Andreas Røsdal <andreasr@src.gnome.org>
2007-01-13 23:31:57 +0000

    Update copyrights with the year 2007.
    svn path=/trunk/; revision=5859

--- gyahtzee.c
+++ gyahtzee.c
@@ -526,7 +526,7 @@ about_cb (GtkAction * action, gpointer data)
   gtk_show_about_dialog (GTK_WINDOW (window),
 			 "name", appName,
 			 "version", VERSION,
-			 "copyright", "Copyright \xc2\xa9 1998-2006 "
+			 "copyright", "Copyright \xc2\xa9 1998-2007 "
 			 "Free Software Foundation, Inc.",
 			 "license", license,
 			 "comments", _("A variation on poker with "
