460db1454e2dfd79e6ae10cf446b39aa6fffadf1
Thomas Hindoe Paaboel Andersen <thomashpa@src.gnome.org>
2008-10-15 13:27:34 +0000

    more libgnome removal and enable gtali to be build with --with-platform=gtk-only
    svn path=/trunk/; revision=8048

--- clist.c
+++ clist.c
@@ -27,8 +27,9 @@
  *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 #include <config.h>
+
+#include <gtk/gtk.h>
 #include <gdk/gdk.h>
-#include <gnome.h>
 
 #include <stdio.h>
 
--- setup.c
+++ setup.c
@@ -30,7 +30,8 @@
  *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 #include <config.h>
-#include <gnome.h>
+
+#include <gtk/gtk.h>
 
 #include <games-frame.h>
 #include <games-conf.h>
