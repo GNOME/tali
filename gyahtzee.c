f3f4bed2be18fa63dd3049131e7d6406ea4e5334
Thomas Hindoe Paaboel Andersen <thomashpa@src.gnome.org>
2008-10-27 00:16:13 +0000

    move common help functionality to libgames-support/games-help.[ch]
    svn path=/trunk/; revision=8205

--- gyahtzee.c
+++ gyahtzee.c
@@ -44,6 +44,7 @@
 #include <gtk/gtk.h>
 #include <gdk/gdkkeysyms.h>
 
+#include <games-help.h>
 #include <games-stock.h>
 #include <games-scores.h>
 #include <games-scores-dialog.h>
@@ -698,26 +699,7 @@ update_undo_sensitivity (void)
 static void
 help_cb (GtkAction * action, gpointer data)
 {
-  GdkScreen *screen;
-  GError *error = NULL;
-
-  screen = gtk_widget_get_screen (GTK_WIDGET (window));
-  gtk_show_uri (screen, "ghelp:gtali", gtk_get_current_event_time (), &error);
-
-  if (error != NULL)
-  {
-    GtkWidget *d;
-    d = gtk_message_dialog_new (GTK_WINDOW (window), 
-                              GTK_DIALOG_MODAL | GTK_DIALOG_DESTROY_WITH_PARENT,
-                              GTK_MESSAGE_ERROR, GTK_BUTTONS_CLOSE, 
-                              "%s", _("Unable to open help file"));
-    gtk_message_dialog_format_secondary_text (GTK_MESSAGE_DIALOG (d),
-                              "             %s", error->message);
-    g_signal_connect (d, "response", G_CALLBACK (gtk_widget_destroy), NULL);
-    gtk_window_present (GTK_WINDOW (d));
-
-    g_error_free (error);
-  }
+  games_help_display (window, "gtali", NULL);
 }
 
 
