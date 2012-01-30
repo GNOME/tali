7876e8ba180d2ac6c9336bc7bd2c2d082f691d03
Robert Ancell <robert.ancell@canonical.com>
2012-01-30 12:22:00 +1100

    Correctly initialize gettext

--- src/gyahtzee.c
+++ src/gyahtzee.c
@@ -878,6 +878,11 @@ main (int argc, char *argv[])
   gboolean retval;
   GError *error = NULL;
 
+  setlocale (LC_ALL, "");
+  bindtextdomain (GETTEXT_PACKAGE, LOCALEDIR);
+  bind_textdomain_codeset (GETTEXT_PACKAGE, "UTF-8");
+  textdomain (GETTEXT_PACKAGE);
+
   games_scores_startup ();
 
   /* Reset all yahtzee variables before parsing args */
