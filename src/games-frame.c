GNOME/tali

commit 3007de9ec0d2253727bb9ae54f304488dfb74b01

GamesFrame: Replace GObject boilerplace with G_DECLARE_FINAL_TYPE
We don't subclass this, so mark it as final for that and therefore
also drop the private members handling, as it's all private.

src/games-frame.c
@@ -26,12 +26,12 @@ enum {
   PROP_LABEL
 };
 
-struct GamesFramePrivate {
+struct _GamesFrame {
   GtkWidget *label;
   GtkWidget *alignment;
 };
 
-G_DEFINE_TYPE_WITH_PRIVATE (GamesFrame, games_frame, GTK_TYPE_BOX);
+G_DEFINE_TYPE (GamesFrame, games_frame, GTK_TYPE_BOX);
 
 static void
 games_frame_init (GamesFrame * frame)
@@ -39,31 +39,30 @@ games_frame_init (GamesFrame * frame)
   GtkBox *box = GTK_BOX (frame);
   PangoAttrList *attr_list;
   PangoAttribute *attr;
-  GamesFramePrivate *priv = games_frame_get_instance_private (frame);
 
   gtk_box_set_spacing (box, 6);
   gtk_box_set_homogeneous (box, FALSE);
   gtk_orientable_set_orientation (GTK_ORIENTABLE (frame), GTK_ORIENTATION_VERTICAL);
 
-  priv->label = gtk_label_new (NULL);
-  gtk_misc_set_alignment (GTK_MISC (priv->label), 0.0, 0.5);
+  frame->label = gtk_label_new (NULL);
+  gtk_misc_set_alignment (GTK_MISC (frame->label), 0.0, 0.5);
 
   attr_list = pango_attr_list_new ();
   attr = pango_attr_weight_new (PANGO_WEIGHT_BOLD);
   attr->start_index = 0;
   attr->end_index = -1;
   pango_attr_list_insert (attr_list, attr);
-  gtk_label_set_attributes (GTK_LABEL (priv->label), attr_list);
+  gtk_label_set_attributes (GTK_LABEL (frame->label), attr_list);
   pango_attr_list_unref (attr_list);
 
-  priv->alignment = gtk_alignment_new (0.0, 0.0, 1.0, 1.0);
-  gtk_alignment_set_padding (GTK_ALIGNMENT (priv->alignment), 0, 0, 12, 0);
+  frame->alignment = gtk_alignment_new (0.0, 0.0, 1.0, 1.0);
+  gtk_alignment_set_padding (GTK_ALIGNMENT (frame->alignment), 0, 0, 12, 0);
 
-  gtk_box_pack_start (box, priv->label, FALSE, FALSE, 0);
-  gtk_box_pack_start (box, priv->alignment, TRUE, TRUE, 0);
+  gtk_box_pack_start (box, frame->label, FALSE, FALSE, 0);
+  gtk_box_pack_start (box, frame->alignment, TRUE, TRUE, 0);
 
-  gtk_widget_set_no_show_all (priv->label, TRUE);
-  gtk_widget_show (priv->alignment);
+  gtk_widget_set_no_show_all (frame->label, TRUE);
+  gtk_widget_show (frame->alignment);
 }
 
 static void
@@ -86,12 +85,11 @@ games_frame_add (GtkContainer *container,
                  GtkWidget *child)
 {
   GamesFrame *frame = GAMES_FRAME (container);
-  GamesFramePrivate *priv = games_frame_get_instance_private (frame);
 
-  gtk_container_add (GTK_CONTAINER (priv->alignment), child);
+  gtk_container_add (GTK_CONTAINER (frame->alignment), child);
 
-  add_atk_relation (priv->label, child, ATK_RELATION_LABEL_FOR);
-  add_atk_relation (child, priv->label, ATK_RELATION_LABELLED_BY);
+  add_atk_relation (frame->label, child, ATK_RELATION_LABEL_FOR);
+  add_atk_relation (child, frame->label, ATK_RELATION_LABELLED_BY);
 }
 
 static void
@@ -158,16 +156,15 @@ void
 games_frame_set_label (GamesFrame *frame,
                        const char *label)
 {
-  GamesFramePrivate *priv = games_frame_get_instance_private (frame);
   g_return_if_fail (GAMES_IS_FRAME (frame));
 
   if (label) {
-    gtk_label_set_text (GTK_LABEL (priv->label), label);
+    gtk_label_set_text (GTK_LABEL (frame->label), label);
   } else {
-    gtk_label_set_text (GTK_LABEL (priv->label), "");
+    gtk_label_set_text (GTK_LABEL (frame->label), "");
   }
 
-  g_object_set (priv->label, "visible", label && label[0], NULL);
+  g_object_set (frame->label, "visible", label && label[0], NULL);
 
   g_object_notify (G_OBJECT (frame), "label");
 }

src/games-frame.h
@@ -25,23 +25,8 @@
 G_BEGIN_DECLS
 
 #define GAMES_TYPE_FRAME            (games_frame_get_type ())
-#define GAMES_FRAME(obj)            (G_TYPE_CHECK_INSTANCE_CAST ((obj), GAMES_TYPE_FRAME, GamesFrame))
-#define GAMES_FRAME_CLASS(klass)    (G_TYPE_CHECK_CLASS_CAST ((klass), GAMES_TYPE_FRAME, GamesFrameClass))
-#define GAMES_IS_FRAME(obj)         (G_TYPE_CHECK_INSTANCE_TYPE ((obj), GAMES_TYPE_FRAME))
-#define GAMES_IS_FRAME_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE ((klass), GAMES_TYPE_FRAME))
-#define GAMES_FRAME_GET_CLASS(obj)  (G_TYPE_INSTANCE_GET_CLASS ((obj), GAMES_TYPE_FRAME, GamesFrameClass))
+G_DECLARE_FINAL_TYPE (GamesFrame, games_frame, GAMES, FRAME, GtkBox);
 
-typedef struct GamesFramePrivate GamesFramePrivate;
-
-typedef struct {
-  GtkBox parent_instance;
-} GamesFrame;
-
-typedef struct {
-  GtkBoxClass parent_class;
-} GamesFrameClass;
-
-GType      games_frame_get_type  (void);
 GtkWidget *games_frame_new       (const char *label);
 void       games_frame_set_label (GamesFrame *frame,
                                   const char *label);

commit eff683fc59b6e2a4f32e7f18e53a29c96e7ccc89

GamesFrame: Add missing parent instance in class struct
Fixes: 3007de9ec0d2253727bb9ae54f304488dfb74b01

src/games-frame.c
@@ -27,6 +27,7 @@ enum {
 };
 
 struct _GamesFrame {
+  GtkBox     parent;
   GtkWidget *label;
   GtkWidget *alignment;
 };